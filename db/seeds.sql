5a0cc16519b455689e6d499e352f95922a8732d4
Riley <rileychatwin@outlook.com>
2020-08-06 02:07:15 -0600

    database and table created

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pickle Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Haminater Burger', FALSE);
