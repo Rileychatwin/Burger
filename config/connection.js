dd31e153a3691f557f895c6308b54cd5b1dd2741
Riley <rileychatwin@outlook.com>
2020-08-07 20:12:14 -0600

    code fix

--- config/connection.js
+++ config/connection.js
@@ -10,11 +10,11 @@ var connection = mysql.createConnection({
 
 
 connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
-  
-  module.exports = connection;
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
--- models/burger.js
+++ models/burger.js
@@ -1,37 +1,24 @@
-var express = require("express");
-var router = express.Router();
-var burger = require("../models/burger.js");
+var orm = require("../config/orm.js");
 
-router.get("/", function(req, res) {
-    res.redirect("/burgers");
-  });
-  
-  router.get("/burgers", function(req, res) {
-
-    burger.all(function(burgerData) {
-
-      res.render("index", { burger_data: burgerData });
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
     });
-  });
-  
+  },
+  create: function(name, cb) {
+    orm.create("burgers", [
+      "burger_name", "devoured"
+    ], [
+      name, false
+    ], cb);
+  },
+  update: function(id, cb) {
+    var condition = "id=" + id;
+    orm.update("burgers", {
+      devoured: true
+    }, condition, cb);
+  }
+};
 
-  router.post("/burgers/create", function(req, res) {
-
-    burger.create(req.body.burger_name, function(result) {
-
-      console.log(result);
-      res.redirect("/");
-    });
-  });
-
-  router.put("/burgers/:id", function(req, res) {
-    burger.update(req.params.id, function(result) {
-
-      console.log(result);
-
-      res.sendStatus(200);
-    });
-  });
-  
-  module.exports = router;
-  
+module.exports = burger;
